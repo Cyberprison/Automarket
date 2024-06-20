51be30febe642130bcb2c2b9c46911065f585aa8
whoami <az.khabibullin2018@gmail.com>
2024-06-20 22:47:15 +0500

    создал слой доступа к данным DAL в VS MS 2017, папка простая, библеотека классов(.Net framework) класс ApplicationDbContext, содержащий все компоненты для взаимодействия с БД. Будут сущности, хранящиеся из таблицы, к которым можно будет обращаться класс ApplicationDbContext, пустой конструктор(по умолчанию, без параметров) Поле DbSet Car (не забудь добавить ссылку на слой с объектами и подключить пространство имён) Сущности - это набор данных, которые хранятся в БД, а именно в нужной таблице Регистрация класса AppDbContext, который хранит компоненты для работы с БД
    не знаю где использовать этот объект, вернусь когда почитаю метанит и пойму как пользоваться

---
+++ Automarket.DAL/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Automarket.Domain.Entity;
+
+namespace Automarket.DAL
+{
+    class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            //Database.EnsureCreated(); - создание БД
+            //EnsureDeleted - удаление БД
+        }
+
+        public DbSet<Car> Car { get; set; }
+    }
+}
